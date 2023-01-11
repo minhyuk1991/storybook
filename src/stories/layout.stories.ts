57eb6769e63cb3ff8509052c203f15d817d6d384
minhyuk <minhyuk@ontune.co.kr>
2023-01-12 08:07:23 +0900

    save

--- src/stories/layout.stories.ts
+++ src/stories/layout.stories.ts
@@ -1,16 +1,12 @@
 import { action } from "@storybook/addon-actions";
 
 import Layout from "../components/layout/Layout.svelte";
-let count = 0;
-function handleClick() {
-  count += 1;
-}
+
 export default {
-  title: "Button",
+  title: "Layout",
 };
 
 export const Aa = () => ({
   Component: Layout,
   props: { text: "Hello" },
-  on: { click: handleClick },
 });
