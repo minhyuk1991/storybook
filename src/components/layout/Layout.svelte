dfcb19d9fb3e91a732ddf595c75652d0b95ea187
minhyuk <minhyuk@ontune.co.kr>
2023-01-11 16:47:18 +0900

    save

--- src/components/layout/Layout.svelte
+++ src/components/layout/Layout.svelte
@@ -12,12 +12,18 @@ const clickHandler = (e: EventMouse) => {
 
 <div class="flex min-h-[100vh] w-full items-stretch">
   <Sidebar clickHandler="{clickHandler}" isOpen="{isOpen}" />
-  <div
-    class="{`overflow-hidden flex flex-1  ${
-      isOpen ? 'translate-x-[300px]' : ''
-    }`}">
-    <main class="{`min-h-[calc(100vh-200px-200px)] flex flex-1 `}">
-      <div class="bg-dark-800 pl-[40px] w-full">daaaaaaaaaaaaaaadd</div>
+  <div class="{`overflow-hidden flex flex-1 `}">
+    <main
+      class="{`min-h-[calc(100vh-200px-200px)] flex flex-1 flex-col items-stretch transform  transition-transform duration-1000 ${
+        isOpen ? 'translate-x-[300px]' : ''
+      }`}">
+      <div class="bg-white pl-[40px] w-full text-black text-center font-bold">
+        Main header
+      </div>
+      <div
+        class="bg-dark-800 pl-[40px] w-full flex-1 text-white text-center font-bold">
+        Main contents
+      </div>
       <slot />
     </main>
   </div>
