d6736f1663e0cfb09d78ea1a19a446dfed8a328f
minhyuk <minhyuk@ontune.co.kr>
2023-01-16 12:30:34 +0900

    save

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 
 import { createHtmlPlugin } from 'vite-plugin-html';
-import wasm from 'vite-plugin-wasm';
+// import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    wasm(),
+    // wasm(),
     topLevelAwait(),
     svelte({
       preprocess: [sveltePreprocess({ typescript: true })],
