e1ddaf894eebb14d40a5606be0bc6188b6242af9
minhyuk <minhyuk@ontune.co.kr>
2023-01-16 09:08:28 +0900

    save

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import Button from './components/button/Button.svelte';
   import Layout from './components/layout/Layout.svelte';
   import Radio from './components/radio/Radio.svelte';
-  export const name = 'assd';
 </script>
 
 <Layout>
