ba8154c168d648927fd0b76dba11b28b243d7ae3
minhyuk <minhyuk@ontune.co.kr>
2023-01-16 10:36:48 +0900

    save

--- src/__tests__/button.test.ts
+++ src/__tests__/button.test.ts
@@ -39,4 +39,19 @@ describe('button ', () => {
     expect(buttonEl.classList.contains('lg')).toBe(false);
     expect(buttonEl.classList.contains('text-[20px]')).toBe(false);
   });
+
+  it('button_props_test: color', () => {
+    render(Button, {
+      props: {
+        text: 'testButton',
+        size: 'sm',
+        color: '#ffffff',
+        onClickF: () => {},
+      },
+    });
+    const buttonEl = screen.getByText('testButton');
+    console.log(buttonEl);
+    expect(buttonEl).toBeInTheDocument();
+    expect(buttonEl.style.backgroundColor === '#ffffff').toBe(true);
+  });
 });
