610d6d8578c3a49149cf36cc37b8494664f9bc53
minhyuk <minhyuk@ontune.co.kr>
2023-02-22 10:58:40 +0900

    save

--- .routify/config.js
+++ .routify/config.js
@@ -4,16 +4,15 @@ module.exports = {
   "routifyDir": ".routify",
   "ignore": "",
   "dynamicImports": true,
-  "singleBuild": false,
+  "singleBuild": true,
   "noHashScroll": false,
   "distDir": "dist",
   "hashScroll": true,
-  "childProcess": "dev-vite",
   "extensions": [
     "html",
     "svelte",
     "md",
     "svx"
   ],
-  "started": "2023-02-21T11:10:20.689Z"
+  "started": "2023-02-22T01:58:10.259Z"
 }
